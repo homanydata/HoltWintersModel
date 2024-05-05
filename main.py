e1e2684c272f6e19111e64614f4de28bc5abd536
homanydata <houmani.datalu@gmail.com>
2024-05-05 16:12:19 +0300

    version 0.7
    changes made:
    - fixed a mistake in S formula in the second loop (froecasting)
    - completed the class, now it is usable with any dataset (several minor errors were fixed)
    - added displayModelComponents to make seeing model L, S, T.. easier
    - wrapped up moving average formula into a seperate method
    - forecasted values are no longer rounded
    - gamma is set to 0.002
    - we can calculate score even if we did not forecast yet, no need to raise an error in score(). deprecated
    As a result, for the core data, all values are correct, including S13 but excluding only F13.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import math
+
+mean = lambda x: round(sum(x)/len(x), 2)
+
+def moving_average(data: list[float], n: int):
+    result = [mean(data[i-n//2:i+(n-n//2)]) if len(data) - n//2 > i >= n//2 else None for i in range(len(data))]
+    return result
+
+
 data = [53, 22, 37, 45, 58, 25, 40, 50, 62, 27, 44, 56]
 m = 4
 alpha = 0.2
 beta = 0.3
-gamma = 0.5
+gamma = 0.002
 steps = 4
 L, S, T, F = [[0 for _ in range(len(data) + steps)] for _ in range(4)]
 
 i = 0
-S[i] = round(data[0] / sum(data[:m]), 2)
-L[i] = 156
-T[i] = (sum(data[m:2 * m]) - sum(data[:m])) / 4
-F[i] = data[0]
+S[0] = round(data[0] / sum(data[:m]), 2)
+L[0] = math.ceil(data[0] / S[0])
+T[0] = (sum(data[m:2 * m]) - sum(data[:m])) / 4
+F[0] = data[0]
 
 # existing data
 for i in range(1, len(data)):
@@ -20,16 +29,14 @@ for i in range(1, len(data)):
     F[i] = round((L[i - 1] + T[i - 1]) * S[i], 2)
 # future
 for i in range(len(data), len(data) + steps):
-    F[i] = round((L[i - 1] + T[i - 1]) * S[i - m], 2)
-    L[i] = round(alpha * (F[i] / S[i - m]) + (1 - alpha) * (L[i - 1] + T[i - 1]), 2)
-    T[i] = round(beta * (L[i] - L[i - 1]) + (1 - beta) * T[i - 1], 2)
-    S[i] = round(gamma * (F[i]/L[i]) + (1 + gamma) * S[i - m], 2)
+    S[i] = gamma * (data[i - m] / L[i - m]) + (1 + gamma) * S[i - m]
+    F[i] = (L[i - 1] + T[i - 1]) * S[i]
+    L[i] = alpha * (F[i] / S[i - m]) + (1 - alpha) * (L[i - 1] + T[i - 1])
+    T[i] = beta * (L[i] - L[i - 1]) + (1 - beta) * T[i - 1]
 
 
-# mean = lambda x: round(sum(x)/len(x), 2)
-# n = 3
-# MA = [mean(data[i-n//2:i+(n-n//2)]) if len(data) - n//2 > i >= n//2 else 0 for i in range(len(data))]
 mad = sum([abs(data[i] - F[i]) for i in range(len(data))])
 msd = sum([(data[i] - F[i])**2 for i in range(len(data))])
 mape = 100 * sum([abs(data[i] - F[i])/data[i] for i in range(len(data))])
-print(f'level: {L}\nseasonality: {S}\ntrend: {T}\nforecast: {F}\ndata:{data}\nMA: {MA}\nMAD: {mad}\nMSD:{msd}\nMAPE:{mape}')
+print(f'level: {L}\nseasonality: {S}\ntrend: {T}\nforecast: {F}\ndata:{data}')
+# print(f'\nMAD: {mad}\nMSD:{msd}\nMAPE:{mape}')
--- winters.py
+++ winters.py
@@ -1,12 +1,23 @@
+import math
+
+mean = lambda x: round(sum(x)/len(x), 2)
+
+def moving_average(data: list[float], n: int):
+    result = [mean(data[i-n//2:i+(n-n//2)]) if len(data) - n//2 > i >= n//2 else None for i in range(len(data))]
+    return result
+
 class Winters:
     def __init__(self):
         pass
+
     def train(self, data, m, alpha, beta, gamma):
         if m < 2: raise ValueError('number of seasons must be minimum 2')
-        if not isinstance(data, list) or any(not isinstance(value, float) for value in data):
+        if not isinstance(data, list) or any(not isinstance(value, float | int) for value in data):
             raise ValueError('data must be a list of floats or integers')
         if not (0<=alpha<=1 and 0<=beta<=1 and 0<=gamma<=1):
             raise ValueError('alpha, beta & gamma must be between 0 and 1')
+        if len(data) < 2 * m:
+            raise ValueError('data must be at least 2 times the number of seasons')
 
         self.data = data
         self.nb_seasons = m
@@ -14,42 +25,64 @@ class Winters:
         L, S, T, F = [[0 for _ in range(len(data))] for _ in range(4)]
 
         i = 0
-        S[i] = data[0] / sum(data[:m])
-        L[i] = sum(data[:m])
-        T[i] = (sum(data[m:2 * m]) - sum(data[:m])) / 4
-        F[i] = data[0]
+        S[0] = round(data[0] / sum(data[:m]), 2)
+        L[0] = math.ceil(data[0] / S[0])
+        T[0] = (sum(data[m:2 * m]) - sum(data[:m])) / 4
+        F[0] = data[0]
 
         for i in range(1, len(data)):
-            S[i] = data[i] / sum(data[i//m * m:(i//m + 1) * m])
-            L[i] = alpha * (data[i] / S[i]) + (1 - alpha) * (L[i - 1] + T[i - 1])
-            T[i] = beta * (L[i] - L[i - 1]) + (1 - beta) * T[i - 1]
-            F[i] = (L[i - 1] + T[i - 1]) * S[i]
+            S[i] = round(data[i] / sum(data[i//m * m:(i//m + 1) * m]), 2)
+            L[i] = round(alpha * (data[i] / S[i]) + (1 - alpha) * (L[i - 1] + T[i - 1]), 2)
+            T[i] = round(beta * (L[i] - L[i - 1]) + (1 - beta) * T[i - 1], 2)
+            F[i] = round((L[i - 1] + T[i - 1]) * S[i], 2)
+
         self.components = {'level': L, 'trend': T, 'seasonality': S}
         self.forecasted = F
 
-    def forcast(self, n: int):
+    def forcast(self, n: int) -> list[float]:
         if self.forecasted is None:
             raise RuntimeError('Model is not trained yet. use Winters.train() method before forecasting')
         if not isinstance(n, int) or n < 1:
             raise ValueError('n must be a positive integer')
 
-        L, T, S = [nums.extend([0 for _ in range(n)]) for nums in self.components.values()]
+        L, T, S = [nums + [0 for _ in range(n)] for nums in self.components.values()]
         F = self.forecasted
         m = self.nb_seasons
-        for i in range(len(data), len(data) + n):
-            F.append((L[i - 1] + T[i - 1]) * S[i - m])
-            L[i] = alpha * (F[i] / S[i - m]) + (1 - alpha) * (L[i - 1] + T[i - 1])
-            T[i] = beta * (L[i] - L[i - 1]) + (1 - beta) * T[i - 1]
-            S[i] = gamma * (F[i]/L[i]) + (1 + gamma) * S[i - m]
-        result = F[len(data):]
-        self.forecasted = F[:len(data)]
+        for i in range(len(self.data), len(self.data) + n):
+            S[i] = self.gamma * (self.data[i - m] / L[i - m]) + (1 + self.gamma) * S[i - m]
+            F.append((L[i - 1] + T[i - 1]) * S[i])
+            L[i] = self.alpha * (F[i] / S[i - m]) + (1 - self.alpha) * (L[i - 1] + T[i - 1])
+            T[i] = self.beta * (L[i] - L[i - 1]) + (1 - self.beta) * T[i - 1]
+        result = F[len(self.data):]
+        self.forecasted = F[:len(self.data)]
         return result
-        
+
 
     def score(self):
-        if self.forecasted is None:
-            raise RuntimeError('Model is not trained yet. use Winters.train() method before evaluating it')
         mad = sum([abs(self.data[i] - self.forecasted[i]) for i in range(len(self.data))])
         msd = sum([(self.data[i] - self.forecasted[i])**2 for i in range(len(self.data))])
         mape = 100 * sum([abs(self.data[i] - self.forecasted[i])/self.data[i] for i in range(len(self.data))])
         return {'MAD': mad, 'MSD': msd, 'MAPE': mape}
+
+    def displayModelComponents(self):
+        print( f"""Trained Model Components
+        Level: {self.components['level']}
+        Seasonality: {self.components['seasonality']}
+        Trend: {self.components['trend']}
+        Forecast: {self.forecasted}
+        Data:{self.data}
+        """)
+
+
+
+d = [53, 22, 37, 45, 58, 25, 40, 50, 62, 27, 44, 56]
+
+model = Winters()
+
+model.train(data=d, m=4, alpha=0.2, beta=0.3, gamma=0.002)
+
+model.displayModelComponents()
+
+print(f'Forecasted Values: {model.forcast(n=1)}')
+
+print(f'\n\nModel Evaluation: {model.score()}')
